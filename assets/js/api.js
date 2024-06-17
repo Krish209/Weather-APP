4d1a658f2c2f3b6c64e3bba739cc955ce1152dee
Krish Raj <76595963+Krish209@users.noreply.github.com>
2024-06-17 11:16:11 -0700

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -22,23 +22,22 @@ export const fetchData = function (URL, callback) {
 
 export const url = {
     currentWeather(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
     },
     forecast(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
     },
     airPollution(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
     },
     reverseGeo(lat, lon) {
-        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
     },
 
     /**Fetch data from the server
      * @param {string} query - Search Query 
+     * @returns
      * */
-
-    
 
     geo(query) {
         return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
